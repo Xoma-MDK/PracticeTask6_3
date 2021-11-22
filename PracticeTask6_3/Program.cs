56a3cee7ade1bbff5dae1f4d0a0c2fdc04245a15
Daniil Molotkov <molotkov.58@ya.ru>
2021-11-22 11:21:34 +0700

    Добавьте файлы проекта.

---
+++ PracticeTask6_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PracticeTask6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x: ");
+            Int64 x = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Введите значение y: ");
+            Int64 y = Convert.ToInt64(Console.ReadLine());
+            double a = (Math.Log(Math.Pow(y, - (Math.Sqrt((Math.Abs(x))))))) * (Math.Cos(x) + (Math.Exp((x + y))));
+            Console.WriteLine("{0:F}", a);
+        }
+    }
+}
